6811ed10180beada2ed9e02f6942a01933da5e6b
sparklyballs <sparklyballs@gmail.com>
2015-10-30 03:32:58 +0000

    fix typo and mv default json to /config

--- init/30_set_config.sh
+++ init/30_set_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[[ ! -f /config/wsdata.json ]] && cp /defaults/default.wsdata.json /config/wsdata.json
-[[ ! -L /app/websync/dist/wsdata.json ]] ln -s /config/wsdata.json /app/websync/dist/wsdata.json
+[[ ! -f /config/wsdata.json ]] && mv /app/websync/dist/wsdata.json /config/wsdata.json
+[[ ! -L /app/websync/dist/wsdata.json ]] && ln -s /config/wsdata.json /app/websync/dist/wsdata.json
 
 chown -R abc:abc /app /config
